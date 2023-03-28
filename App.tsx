702a36074c25c1a9a093038e2fea59ac3cd3dd2c
jricardoc <ricardoocarvalhoo10@gmail.com>
2023-03-28 17:44:59 -0300

    styled-components

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
 import { Groups } from "@screens/Groups";
 
+import theme from "@theme/index";
+
+import { ThemeProvider } from 'styled-components'
+
 export default function App() {
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      <Groups />
+    </ThemeProvider>
   );
 }
 
--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -2,11 +2,11 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
     flex: 1;
-    background-color: #000;
+    background-color: ${({theme}) => theme.COLORS.GREEN_500};
     align-items: center;
     justify-content: center;
 `;
 
 export const Title = styled.Text`
-    color: #fff;
+    color: #000;
 `;
