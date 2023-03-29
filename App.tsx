9ed6896cecffe363afa28e03971daa4cadc7d446
jricardoc <ricardoocarvalhoo10@gmail.com>
2023-03-29 17:39:36 -0300

    statusbar

--- App.tsx
+++ App.tsx
@@ -1,13 +1,23 @@
+import { StatusBar } from 'react-native';
 import { Groups } from "@screens/Groups";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import theme from "@theme/index";
+import { Loading } from '@components/Loading';
 
 import { ThemeProvider } from 'styled-components'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -2,11 +2,12 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
     flex: 1;
-    background-color: ${({theme}) => theme.COLORS.GREEN_500};
+    background-color: ${({theme}) => theme.COLORS.RED_DARK};
     align-items: center;
     justify-content: center;
 `;
 
 export const Title = styled.Text`
     color: #000;
+    font-size: 40px;
 `;
