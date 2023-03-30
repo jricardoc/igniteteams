f88ef3d9c230b343a7ddcfbc41f7f0c759bee95b
jricardoc <ricardoocarvalhoo10@gmail.com>
2023-03-30 17:56:05 -0300

    highlight

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,18 @@
 import { Header } from '@components/Header';
 import { Container, Title } from './styles';
+import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
 
 export function Groups() {
   return (
     <Container>
       <Header /> 
+      <Highlight 
+        title='Turmas'
+        subtitle='Jogue com a sua turma'
+      />
+
+      <GroupCard title='Galera do ignite'/>
     </Container>
   );
 }
