5a3d0bbafce47b7535759f78027b9db6e02bea5a
jricardoc <ricardoocarvalhoo10@gmail.com>
2023-03-30 13:02:57 -0300

    header

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -2,9 +2,9 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
     flex: 1;
-    background-color: ${({theme}) => theme.COLORS.RED_DARK};
-    align-items: center;
-    justify-content: center;
+    background-color: ${({theme}) => theme.COLORS.GRAY_600};
+    /* align-items: center; */
+    /* justify-content: center; */
 `;
 
 export const Title = styled.Text`
